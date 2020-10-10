d5e46d83d1f62d66315433248a0faa17d3b80455
ana1155 <anastasiia.solop@softermii.com>
2020-10-10 11:37:48 +0300

    inProgress onClick

--- frontend/src/Layout/TasksInProgress/index.js
+++ frontend/src/Layout/TasksInProgress/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router';
 import { useSelector } from 'react-redux';
 import { Grid } from '@material-ui/core';
+import useStyles from './useStyles';
 
 const TasksInProgress = () => {
   const inProgress = useSelector(state => state.projects.inProgress);
+  const history = useHistory();
+  const classes = useStyles();
 
   return inProgress.length > 0 ? (
     <Grid container>
       {inProgress.map(task => (
         <Grid item key={task.id}>
-          <b> --- {task.body} --- </b>
+          <b
+            onClick={() => history.push(`/projects/${task.project_id}`)}
+            className={classes.taskName}
+          >
+            --- {task.body} ---
+          </b>
         </Grid>
       ))}
     </Grid>
