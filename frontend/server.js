894cc3a936e4510d01168969bf29fd5f9a777730
ana1155 <anastasiia.solop@softermii.com>
2020-10-30 15:32:25 +0200

    remove https

--- frontend/server.js
+++ frontend/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
-// const https = require('https');
-// const http = require('http');
-// const fs = require('fs');
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
 
 const app = express();
 
@@ -16,7 +16,7 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-// const httpServer = http.createServer(app);
+const httpServer = http.createServer(app);
 // const httpsServer = https.createServer(
 //   {
 //     key: fs.readFileSync('./private.key'),
@@ -24,26 +24,11 @@ app.get('/*', (req, res) => {
 //   },
 //   app,
 // );
-//
-// httpServer.listen(80, () => {
-//   console.log('HTTP Server running on port 80');
-// });
+
+httpServer.listen(80, () => {
+  console.log('HTTP Server running on port 80');
+});
 //
 // httpsServer.listen(443, () => {
 //   console.log('HTTPS Server running on port 443');
 // });
-
-require("greenlock-express")
-  .init({
-    packageRoot: __dirname,
-    configDir: "./greenlock.d",
-
-    // contact for security and critical bug notices
-    maintainerEmail: "anastasiya1155@gmail.com",
-
-    // whether or not to run at cloudscale
-    cluster: false
-  })
-  // Serves on 80 and 443
-  // Get's SSL certificates magically!
-  .serve(app);
